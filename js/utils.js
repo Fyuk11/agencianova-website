e696d211a5f13f7f3a0c751a3da4b9fe6d64e46a
Fyuk11 <rodrigomez.11e@gmail.com>
2025-08-19 11:50:49 -0300

    Actualización de estilos

--- js/utils.js
+++ js/utils.js
@@ -1,29 +1,4 @@
-// js/utils.js
-/*export const qs = (s, el = document) => el.querySelector(s);
-export const qsa = (s, el = document) => [...el.querySelectorAll(s)];
-export const on = (el, ev, fn, opts) => el && el.addEventListener(ev, fn, opts);
-
-export const throttle = (fn, limit = 100) => {
-  let inThrottle, lastArgs;
-  return (...args) => {
-    lastArgs = args;
-    if (!inThrottle) {
-      fn(...lastArgs);
-      inThrottle = true;
-      setTimeout(() => {
-        inThrottle = false;
-        if (lastArgs) fn(...lastArgs);
-      }, limit);
-    }
-  };
-};
-
-export const inView = (el, offset = 0) => {
-  const rect = el.getBoundingClientRect();
-  return rect.top <= (window.innerHeight - offset) && rect.bottom >= 0;
-};
- */
-
+// utils.js
 export const qs = (s, el = document) => el.querySelector(s);
 export const qsa = (s, el = document) => [...el.querySelectorAll(s)];
 export const on = (el, ev, fn, opts) => el && el.addEventListener(ev, fn, opts);
@@ -47,3 +22,12 @@ export const inView = (el, offset = 0) => {
   const rect = el.getBoundingClientRect();
   return rect.top <= (window.innerHeight - offset) && rect.bottom >= 0;
 };
+
+
+// utils.js (o al final de main.js)
+/*window.addEventListener("load", () => {
+  if (window.location.hash) {
+    history.replaceState(null, null, window.location.pathname);
+    window.scrollTo({ top: 0, behavior: "instant" });
+  }
+});*/
